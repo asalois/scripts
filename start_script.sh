8e98ae8a4c5b4ec1709be7da8d36dda73986d91d
Alexander Salois <asalois.scholar@gmail.com>
2020-02-20 10:23:53 -0700

    added a wait

--- start_script.sh
+++ start_script.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+sleep 5 # wait until you get to the desktop
 konsole --hold -e "~/scripts/start_xps.sh" & # update and start up xps
 konsole --hold -e "htop" & # show me what is going on
 firefox &
