9fbd013768f259ec7883fc3b50e828b1169314fd
Alexander Salois <asalois.scholar@gmail.com>
2020-02-21 14:28:55 -0700

    start scripts talk better

--- start_script.sh
+++ start_script.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-sleep 5 # wait until you get to the desktop
-konsole --hold -e "~/scripts/start_xps.sh" & # update and start up xps
 konsole --hold -e "htop" & # show me what is going on
+~/scripts/start_xps.sh # update and start up xps
 firefox &
