f216385ea86fa315e17fca2d685206ef9746629e
Alexander Salois <asalois.scholar@gmail.com>
2020-03-10 19:49:30 -0600

    added cases to find failed jobs

--- find_cancelled_1.sh
+++ find_cancelled_1.sh
@@ -2,3 +2,8 @@
 # find the files that got canceled then print them to failed_files
 grep "CANCELLED" *err.txt | cut -d . -f 1 |cut -d _ -f 3 
 grep "insufficient memory"* *.txt | cut -d . -f 1 |cut -d _ -f 3 
+grep "Abnormal termination"* *.txt | cut -d . -f 1 |cut -d _ -f 3
+grep "Segmentation fault"* *.txt | cut -d . -f 1 |cut -d _ -f 3
+grep "Resource temporarily unavailable"* *.txt | cut -d . -f 1 |cut -d _ -f 3
+find -L *err.txt  -maxdepth 1  -type f ! -size 0 | cut -d . -f 1 |cut -d _ -f 3
+
