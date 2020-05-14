e2774040b6a787d244b446a134b2c93885198209
Alex Salois <asalois.scholar@gmail.com>
2020-05-14 16:20:20 -0600

    updated to work with ubuntu

--- update.sh
+++ update.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 sudo apt update         # have the newest packages ready
-sudo pkcon update -y    # kde update
+sudo apt upgrade -y    # kde update
 sudo snap refresh       # update snap packages
 sudo apt autoremove     # clean up unsused packages
