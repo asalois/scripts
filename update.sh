34c06e4e62ee1c496941f0601d4143cb998ee62e
Alex Salois <asalois.scholar@gmail.com>
2020-05-15 11:20:30 -0600

    added comment

--- update.sh
+++ update.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 sudo apt update         # have the newest packages ready
-sudo apt upgrade -y    # kde update
+sudo apt upgrade -y     # upgrade packages
 sudo snap refresh       # update snap packages
 sudo apt autoremove     # clean up unsused packages
