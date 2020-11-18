1bc2fe34208467cb87fe7e36f2c5f81c6c70c94f
Alexander Salois <asalois.scholar@gmail.com>
2020-11-18 16:36:28 -0700

    update no longer writes on itself

--- update.sh
+++ update.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 sudo apt update         # have the newest packages ready
-sudo pkcon update || sudo apt upgrade -y     # upgrade packages
+sudo pkcon update -y
 sudo snap refresh       # update snap packages
-sudo apt autoremove     # clean up unsused packages
+sudo apt autoremove -y     # clean up unsused packages
