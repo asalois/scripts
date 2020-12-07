39a12712dbd2aa0315bb6c351c1f28ef8b388435
Alex Salois <asalois.scholar@gmail.com>
2020-12-07 15:43:03 -0700

    now using fedora + no longer needed

--- start_script.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-konsole --hold -e "htop" & # show me what is going on
-~/scripts/start_xps.sh # update and start up xps
-firefox &
--- update.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-sudo apt update         # have the newest packages ready
-sudo pkcon update -y
-sudo snap refresh       # update snap packages
-sudo apt autoremove -y     # clean up unsused packages
