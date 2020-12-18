e4ba461504e352dc0490b7217c9e4253cd134d1f
Alex Salois <asalois.scholar@gmail.com>
2020-12-18 09:47:42 -0700

    download and set up conda for tensorflow

--- set_up_tf.sh
+++ set_up_tf.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
+wget https://repo.anaconda.com/archive/Anaconda3-2020.11-Linux-x86_64.sh
+chmod +x Anaconda3-2020.11-Linux-x86_64.sh
+./Anaconda3-2020.11-Linux-x86_64.sh
 conda create -n tf scipy keras tensorflow
 conda create -n tf-gpu scipy keras tensorflow-gpu
